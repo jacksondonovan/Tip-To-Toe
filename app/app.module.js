2f0633756f8679b14184b13b9104e68ce2c7352e
Jackson Donovan <iinventedtwitter@gmail.com>
2017-08-29 13:32:45 -0600

    content on all views, WIP solving injector error

---
+++ app/app.module.js
@@ -0,0 +1,32 @@
+var app = angular.module('app',["ngRouter"])
+
+// app.component('home',{
+//   controller: function($scope){
+//
+//   },
+//   templateUrl: '/templates/home.hbs'
+// })
+
+app.controller('homedisplay',function($scope){
+  $scope.message = 'JACKSON DONOVAN'
+})
+
+app.config(["$routeProvider",function($routeProvider){
+  $routeProvider
+   .when('/home',{
+     templateUrl: '/templates/home.hbs',
+     contoller: 'homedisplay'
+   })
+   .when('/about',{
+     templateUrl: '/templates/about.hbs'
+   })
+   .when('/photos',{
+     templateUrl: '/templates/photos.hbs'
+   })
+   .when('/contact',{
+     templateUrl: '/templates/contact.hbs'
+   })
+   .otherwise({
+     redirectTo: '/home'
+   })
+}])
