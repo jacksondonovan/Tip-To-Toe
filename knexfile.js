1a102c105cf04c5510ccba99e7fbaa2ce75e0e7c
Jackson Donovan <iinventedtwitter@gmail.com>
2017-08-31 16:22:34 -0600

    getting heroku db connection set up

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 module.exports = {
 
   development: {
@@ -6,10 +8,6 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: {
-      database:'postgres://eqaxxqsmbavpal:41cc04a94c25e2c958922ebdbd6efe477ed52df3ea2527d41acf033fb6c7a158@ec2-54-163-233-201.compute-1.amazonaws.com:5432/d33sqlagfcs7hs',
-      user:     'username',
-      password: 'password'
-    }
+    connection:'postgres://eqaxxqsmbavpal:41cc04a94c25e2c958922ebdbd6efe477ed52df3ea2527d41acf033fb6c7a158@ec2-54-163-233-201.compute-1.amazonaws.com:5432/d33sqlagfcs7hs'
   }
 }
